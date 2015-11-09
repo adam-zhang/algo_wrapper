b447471331e6585e714d0828076c664aa2cf53e0
mingpingzhang <mingpingzhang@163.com>
2015-11-09 09:41:03 +0800

    inlined functions

--- algo_wrapper.hpp
+++ algo_wrapper.hpp
@@ -3,13 +3,13 @@
 
 #include <algorithm>
 
-template<typename Container>
+template<typename Container> inline
 typename Container::iterator adjacent_find(Container& container)
 {
 	return adjacent_find(container.begin(), container.end());
 }
 
-template<typename Container, typename T>
+template<typename Container, typename T> inline
 typename Container::iterator find(Container& container, const T& value)
 {
 	return std::find(container.begin(), container.end(), value);
